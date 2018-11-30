fbb04f98d1858ea8bfd013682fe48e164b8a815b
Ashley Trinh <ashley.trinh@hackbrightacademy.com>
2018-11-30 09:11:59 -0800

    Refactor cprint

--- util/cprint.py
+++ util/cprint.py
@@ -4,23 +4,52 @@ Things for printing messages.
 
 
 def cprint(s, *msg_types, indent=0):
-    styles = [getattr(colors, t, '') for t in msg_types]
-    end = colors.ENDC if msg_types else ''
-    indent = ' ' * indent
+    """Print things with colors."""
 
-    print(''.join(styles) +
-          indent +
-          s +
-          end
-          )
+    # Right now, this only works with foreground colors and styles
+
+    # Get ANSI escape sequence from colors class or styles class
+    # Default to empty string if invalid
+    valid_ansi = [getattr(colors.fg, t, getattr(styles, t, ""))
+                  for t in msg_types
+                  ]
+
+    # If there were no valid styles (the styles list is empty), then we don"t
+    # need to add a reset to the end of our print statement
+    end = colors.end if valid_ansi else ""
+
+    indent = "    " * indent
+
+    print("".join(valid_ansi) + indent + s + end)
 
 
-class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+class _ansi_global:
+    end = "\033[0m"
+
+
+class colors(_ansi_global):
+    class fg:
+        black = "\033[30m"
+        red = "\033[31m"
+        green = "\033[32m"
+        yellow = "\033[33m"
+        blue = "\033[34m"
+        magenta = "\033[35m"
+        cyan = "\033[36m"
+        white = "\033[37m"
+
+    class bg:
+        black = "\033[40m"
+        red = "\033[41m"
+        green = "\033[42m"
+        yellow = "\033[43m"
+        blue = "\033[44m"
+        magenta = "\033[45m"
+        cyan = "\033[46m"
+        white = "\033[47m"
+
+
+class styles(_ansi_global):
+    bold = "\033[1m"
+    underline = "\033[4m"
+
